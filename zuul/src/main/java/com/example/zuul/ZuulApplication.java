a82310d70e8ae2aafe5a8264cba2d4f29d17d4a9
maeluenie <maeluenie@gmail.com>
2020-07-14 10:07:21 +0700

    zuul

---
+++ zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.example.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
